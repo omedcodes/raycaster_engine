bc53cd3362999320d3891ffd819301c299b2c71e
omedcodes <omedsafi21@gmail.com>
2026-03-06 17:20:24 +0100

    Feat implement player rendering function

--- raycast.c
+++ raycast.c
@@ -4,34 +4,58 @@
 #define WIDTH 900
 #define HEIGHT 600
 
+#define PLAYER_FOV 80
+
 #define COLOR_WHITE 0xFFFFFFFF
+#define PLAYER_COLOR 0xFF00FF00
+#define CELL_SIZE 50
 
-int main(int argc, char *argv[]) {
-  SDL_Init(SDL_INIT_VIDEO);
-  SDL_Window *window =
-      SDL_CreateWindow("Raycasting Engine", SDL_WINDOWPOS_CENTERED,
-                       SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
+int map[5][5] = {
+    {0, 0, 0, 1, 0},
+    {0, 0, 1, 1, 0},
+    {1, 0, 0, 1, 1},
+    {1, 0, 0, 0, 1},
+    {1, 1, 1, 0, 1},
+};
 
-  SDL_Surface *surface = SDL_GetWindowSurface(window);
+typedef struct {
+    float x;
+    float y;
+    float angle;
+} Player;
 
-  bool running = true;
-  int counter = 0;
-  while (running) {
-    SDL_Event event;
-    while (SDL_PollEvent(&event)) {
-      switch (event.type) {
-      case SDL_QUIT:
-        running = false;
-        break;
-      }
+Player player = { .x = 0.0f, .y = 0.0f, .angle = 45.0f };
+
+void drawPlayer(SDL_Surface* surface, Player p) {
+    SDL_Rect player_rect = { .x = (int)p.x, .y = (int)p.y, .w = 10, .h = 10 };
+    SDL_FillRect(surface, &player_rect, PLAYER_COLOR);
+}
+
+int main(int argc, char* argv[]) 
+{
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_Window* window = SDL_CreateWindow("Raycasting Engine",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
+
+    SDL_Surface* surface = SDL_GetWindowSurface(window);
+    bool running = true;
+
+    while (running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+        SDL_FillRect(surface, NULL, 0x00000000);
+        drawPlayer(surface, player);
+
+        SDL_UpdateWindowSurface(window);
+        SDL_Delay(16);
     }
-    SDL_Rect rect = {WIDTH / 2, HEIGHT / 2, 100 + counter++, 100 + counter++};
-    SDL_FillRect(surface, &rect, COLOR_WHITE);
 
-    SDL_UpdateWindowSurface(window);
-    SDL_Delay(10);
-  }
-
-  SDL_Quit();
-  return 0;
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 }
